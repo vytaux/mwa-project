3d889f1fb096da932d1fed11b1168560d5909ca4
Aayush Raj Joshi <joshiaayushraj@gmail.com>
2024-04-20 20:47:00 -0500

    workspace component initialized

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from './signup/signup.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component:SignupComponent },
+    { path: 'workspace', component: WorkspaceComponent },
 ];
