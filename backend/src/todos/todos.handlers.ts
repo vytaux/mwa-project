c9ae55a4a5d2ba43fe52ce6572f01ce30e201b71
Aayush Raj Joshi <joshiaayushraj@gmail.com>
2024-04-21 17:11:21 -0500

    todo functionlaity on progress

--- backend/src/todos/todos.handlers.ts
+++ backend/src/todos/todos.handlers.ts
@@ -1,17 +1,25 @@
 import { RequestHandler } from "express";
 import { StandardResponse } from "../helpers/types";
 import { Todo, TodoModel } from "./todos.model";
+import { WorkspaceModel } from "../workspaces/workspaces.model";
 
-export const getTodos: RequestHandler<unknown, StandardResponse<Todo[]>, unknown, { page: number; }> = async (req, res, next) => {
+export const getTodos: RequestHandler<{ workspaceId: string }, StandardResponse<any>, unknown, { page: number; }> = async (req, res, next) => {
     try {
         const pageSize = 10;
         const page = req.query.page || 1;
         const userId = req.token._id;
+        const workspaceId = req.params.workspaceId;
 
-        // User can only access their own todos
-        const results = await TodoModel.find({
-            'created_by.user_id': userId
-        })
+        const results = await WorkspaceModel.findOne(
+            {
+                _id: workspaceId,
+                $or: [
+                    { owner_id: userId },
+                    { members: { $elemMatch: { user_id: userId } } }
+                ]
+            },
+            { _id: 0, members: 1 }
+        )
             .skip((page - 1) * pageSize)
             .limit(pageSize);
 
@@ -21,8 +29,9 @@ export const getTodos: RequestHandler<unknown, StandardResponse<Todo[]>, unknown
     }
 }
 
-export const getTodoById: RequestHandler<{ todoId: string }, StandardResponse<Todo | null>> = async (req, res, next) => {
+export const getTodoById: RequestHandler<{ workspaceId: string, todoId: string }, StandardResponse<Todo | null>> = async (req, res, next) => {
     try {
+        const workspaceId = req.params.workspaceId;
         const todoId = req.params.todoId;
         const todo = await TodoModel.findOne({ _id: todoId });
         res.status(200).json({ success: true, data: todo });
@@ -31,17 +40,36 @@ export const getTodoById: RequestHandler<{ todoId: string }, StandardResponse<To
     }
 }
 
-export const postTodo: RequestHandler<unknown, StandardResponse<string>, Todo> = async (req, res, next) => {
+export const postTodo: RequestHandler<{ workspaceId: string }, StandardResponse<number>, Todo> = async (req, res, next) => {
     try {
         const newTodo = req.body;
         const { _id, email } = req.token;
+        const workspaceId = req.params.workspaceId;
+        const userId = req.token._id;
 
-        const created = await TodoModel.create({
-            ...newTodo,
-            created_by: { user_id: _id, email }
-        });
+        const created = await WorkspaceModel.updateOne(
+            {
+                _id: workspaceId,
+                $or: [
+                    { owner_id: userId },
+                    { members: { $elemMatch: { user_id: userId } } }
+                ]
+            },
+            {
+                $push: {
+                    todos: {
+                        ...newTodo,
+                        created_by: { user_id: _id, email }
+                    }
+                }
+            }
+        );
 
-        res.status(200).json({ success: true, data: created._id.toString() });
+        if (created.modifiedCount < 1) {
+            throw new Error("Error while adding todo");
+        }
+
+        res.status(200).json({ success: true, data: created.modifiedCount });
     } catch (error) {
         next(error);
     }
