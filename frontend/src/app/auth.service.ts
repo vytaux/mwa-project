0d8df6501ccf2dc962c8bbb1f453e9f2f2b5aa18
Aayush Raj Joshi <joshiaayushraj@gmail.com>
2024-04-21 13:42:21 -0500

    linear login feature

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,9 +1,57 @@
-import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Injectable, effect, inject, signal } from '@angular/core';
+import { environment } from './../environments/environment.development'
+ 
+export interface User {
+  id: string;
+  email: string;
+  password: string;
+}
+ 
+export interface loginResponse {
+  success: boolean;
+  data: {
+    email: string;
+    token: string;
+  };
+}
+ 
+export interface State {
+  email: string;
+  token: string;
+}
+ 
+export interface Token {
+  token: string;
+}
+ 
+export const initialState = {
+  email: 'guest@todo.mania',
+  token: '',
+};
+ 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor() { }
+  constructor() {
+    effect(() => {
+      localStorage.setItem('State', JSON.stringify(this.$state()));
+    });
+  }
+ 
+  readonly #http = inject(HttpClient);
+  $state = signal<State>(initialState);
+ 
+  login(credentials: { email: string; password: string }) {
+    return this.#http.post<loginResponse>(
+      environment.apiUrl+'api/v1/login',
+      credentials
+    );
+  }
+ 
+  is_logged_in() {
+    return this.$state().token ? true : false;
+  }
 }
+ 
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html'
 })
+
 export class LoginComponent {
+  auth = inject(AuthService);
+  router = inject(Router);
+
+  form = inject(FormBuilder).nonNullable.group({
+    email: ['sam@gmail.com', Validators.required],
+    password: ['thispassword', Validators.required],
+  });
+
+  login() {
+    this.auth
+      .login(this.form.value as { email: string; password: string })
+      .subscribe({
+        next: (res) => {
+          if (!res.success) {
+            throw new Error('Login failed');
+          }
+          this.auth.$state.set({
+            email: res.data.email,
+            token: res.data.token,
+          });
+          this.router.navigate(['/workspace']);
+        },
+        error: (error) => {
+          this.router.navigate(['/register']);
+        }
+      });
+  }
 
 }
