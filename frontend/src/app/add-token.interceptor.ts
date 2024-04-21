129bdcf8ca03b960b600e7acdc8d7a8fdaeeca09
Aayush Raj Joshi <joshiaayushraj@gmail.com>
2024-04-21 13:49:05 -0500

    interceptor added

--- frontend/src/app/add-token.interceptor.ts
+++ frontend/src/app/add-token.interceptor.ts
@@ -1,5 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
 
 export const addTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const auth = inject(AuthService);
+  if (auth.$state().token) {
+    const reqWithToken = req.clone(
+      { headers: req.headers.set('Authorization', 'Bearer ' + auth.$state().token) }
+    );
+    return next(reqWithToken);
+  } else {
+    return next(req);
+  }
 };
