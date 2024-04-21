8be6823c6e306444b85a7a6b13e0646124467649
Aayush Raj Joshi <joshiaayushraj@gmail.com>
2024-04-20 19:21:31 -0500

    login and sigup component initialized

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
 @Component({
