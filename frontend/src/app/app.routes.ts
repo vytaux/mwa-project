54cf3841eacf5cce0efb65605d5f5dfdc0c5c0d9
Vytautas A <vytautasasm@gmail.com>
2024-04-23 17:34:03 -0500

    fixes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 export const routes: Routes = [
   { path: '', component: HomepageComponent, canMatch: [AuthService], pathMatch: 'full' },
   { path: '', component: WorkspaceComponent, canActivate: [AuthService], pathMatch: 'full' },
+  { path: 'w/:workspaceId', component: WorkspaceComponent, canActivate: [AuthService] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'w/:workspaceId', component: WorkspaceComponent, canActivate: [AuthService] },
 ];
