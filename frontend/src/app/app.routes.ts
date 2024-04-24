cf9c81c43d71647b37d3ca85c4c8d565231816d3
Vytautas A <vytautasasm@gmail.com>
2024-04-23 19:44:22 -0500

    lazy load routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,15 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
-import { RegisterComponent } from './register/register.component';
-import { WorkspaceComponent } from './workspace/workspace.component';
 import { AuthService } from './auth.service';
-import { HomepageComponent } from './homepage/homepage.component';
+
+const loadHomepageComponent = () => import('./homepage/homepage.component').then(c => c.HomepageComponent);
+const loadWorkspaceComponent = () => import('./workspace/workspace.component').then(c => c.WorkspaceComponent);
+const loadLoginComponent = () => import('./login/login.component').then(c => c.LoginComponent);
+const loadRegisterComponent = () => import('./register/register.component').then(c => c.RegisterComponent);
 
 export const routes: Routes = [
-  { path: '', component: HomepageComponent, canMatch: [AuthService], pathMatch: 'full' },
-  { path: '', component: WorkspaceComponent, canActivate: [AuthService], pathMatch: 'full' },
-  { path: 'w/:workspaceId', component: WorkspaceComponent, canActivate: [AuthService] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: '', loadComponent: loadHomepageComponent, canMatch: [AuthService], pathMatch: 'full' },
+  { path: '', loadComponent: loadWorkspaceComponent, canActivate: [AuthService], pathMatch: 'full' },
+  { path: 'w/:workspaceId', loadComponent: loadWorkspaceComponent, canActivate: [AuthService] },
+  { path: 'login', loadComponent: loadLoginComponent },
+  { path: 'register', loadComponent: loadRegisterComponent },
 ];
